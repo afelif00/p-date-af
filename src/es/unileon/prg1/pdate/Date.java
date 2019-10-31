03b95eb6431b98f5ca4707f6927c6c5334c324a3
andres <andresforerorivera@gmail.com>
2019-10-31 12:28:29 +0100

    modificando

--- src/es/unileon/prg1/pdate/Date.java
+++ src/es/unileon/prg1/pdate/Date.java
@@ -1,18 +1,45 @@
 package es.unileon.prg1.pdate;
-
+/**
+ * implementacion de la clase DATE para la clase 1
+ * @author andres
+ *
+ */
 public class Date {
 	private int day;
 	private int month;
 	private int year;
 
-	Date(int day, int month, int year){
-		this.day;
-		this.month;
-		this.year;
+	Date(){
+		this.day = 1;
+		this.month= 1;
+		this.year = 2020;
+		}
+	/**
+	 * 
+	 * @param day
+	 * @param month
+	 * @param year
+	 */
+		
+	public Date(int day, int month, int year){
+		this.day = day;
+		this.month= month;
+		this.year = year;
 	}
+	public boolean isSameYear(Date miFecha) {
+		boolean remote = false;
+		if(this.year=miFecha.getYear());
+	}
+	public boolean isSameYear(int miYear) {
+		boolean remote = false;
+		if(this.year == miYeaer)
+			retorno = true;
+		System.out.println("opcion Enteros");
+		return retorno;
+	
 	
 	public String toString() {
-		return ""
+		return "DATE[day"+day+", month="+month+", year="+year+"]";
 	}
 	
 		
--- src/es/unileon/prg1/pdate/pdaPrincipal.java
+++ src/es/unileon/prg1/pdate/pdaPrincipal.java
@@ -1,14 +1,27 @@
 package es.unileon.prg1.pdate;
-
+/**
+ * 
+ * @author andres
+ *
+ */
 public class pdaPrincipal {
-
-	public pdaPrincipal() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void main(String[] args) {
+		Date miFecha = new Date();
+		Date miFecha2 = new Date(1,11,2020);
+		
+		System.out.println(miFecha);
+		System.out.println(miFecha2);
+		
+		if(miFecha.isSameYear(miFecha2))
+			System.out.println("Es el mismo año");
+		else
+			System.out.println("Es distinto año");
+		
+		if(miFecha.isSameYear(miFecha2.get))
+			System.out.println("Es el mismo año");
+		else
+			System.out.println("Es distinto año");
+		
 	}
 
 }
