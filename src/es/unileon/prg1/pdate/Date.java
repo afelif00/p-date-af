e9b6fb11eb256f1d198ae5bb1e7876c52614bda2
andres <andresforerorivera@gmail.com>
2019-10-31 11:44:59 +0100

    inicial

---
+++ src/es/unileon/prg1/pdate/Date.java
@@ -0,0 +1,20 @@
+package es.unileon.prg1.pdate;
+
+public class Date {
+	private int day;
+	private int month;
+	private int year;
+
+	Date(int day, int month, int year){
+		this.day;
+		this.month;
+		this.year;
+	}
+	
+	public String toString() {
+		return ""
+	}
+	
+		
+	
+}
---
+++ src/es/unileon/prg1/pdate/pdaPrincipal.java
@@ -0,0 +1,14 @@
+package es.unileon.prg1.pdate;
+
+public class pdaPrincipal {
+
+	public pdaPrincipal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
